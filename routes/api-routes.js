3a39e3cc378c00fa4dcb43212dda1d0cc9a86909
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-02 15:32:42 +0100

    add routes for sing-in POST and sign-out GET

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,4 +5,10 @@ const user_controller = require('../controllers/userController');
 //POST user sign-up form 
 router.post('/sign-up', user_controller.sign_up_post)
 
+//POST user sign-in 
+router.post('/', user_controller.sign_in_post)
+
+//GET user sign-out
+router.get('/sign-out', user_controller.sign_out_get)
+
 module.exports = router;
