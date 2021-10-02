955de8354069864cae5c0d14af8d33b9af9dc29e
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-02 13:23:14 +0100

    added CommentSchema

---
+++ models/comment.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+    postId: { type: Schema.Types.ObjectId, ref: "Post" },
+    userId: { type: Schema.Types.ObjectId, ref: "User" },
+    text: { type: String, minLength: 1, required: true },
+    date: { type: Date, default: Date.now },
+    likes: [{ type: Schema.Types.ObjectId, ref: "User" }]
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
