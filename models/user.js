f29faff9e2ef3d6fb7a381c7b238dede28fe27ed
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-02 14:57:06 +0100

    refactor

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
-    profilePicUrl: { type: String, required: true },
+    profilePicUrl: { type: String },
     friends: [{ type: Schema.Types.ObjectId, ref: "User" }]
 });
 
