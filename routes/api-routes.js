75c347fc28eb054a6b30e2558fee09baccd1f0d6
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-02 16:37:38 +0100

    added route to POST new_post

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const user_controller = require('../controllers/userController');
+const post_controller = require('../controllers/postController');
 
+/*
+    USERS ROUTES
+*/
 //POST user sign-up form 
 router.post('/sign-up', user_controller.sign_up_post)
 
@@ -10,5 +14,11 @@ router.post('/', user_controller.sign_in_post)
 
 //GET user sign-out
 router.get('/sign-out', user_controller.sign_out_get)
+
+/*
+    POSTS ROUTES
+*/
+//POST new post
+router.post('/:id', post_controller.new_post)
 
 module.exports = router;
