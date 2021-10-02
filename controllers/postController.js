8b189b026205932b06e27f75a2003302eb2eea1f
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-02 18:37:54 +0100

    add update for user post

--- controllers/postController.js
+++ controllers/postController.js
@@ -22,10 +22,46 @@ exports.new_post = [
             return
         } else {
             //save post to database
+            console.log(req.body)
             post.save(function (err) {
                 if (err) { return next(err) }
-                res.json({ alerts: { msg: "Post Saved Successfully" } })
+                res.json({ alerts: [{ msg: "Post Saved Successfully" }] })
             });
+        };
+    }
+];
+
+// Handle user update post
+exports.update_post = [
+
+    body('text', 'Text Must Not Be Empty').trim().isLength({ min: 1 }),
+
+    (req, res, next) => {
+        //Errors from req if any
+        const errors = validationResult(req);
+
+        const { text } = req.body;
+
+        const post = new Post({
+            _id: req.params.postid,
+            userId: req.params.userId,
+            text,
+        })
+
+        if (!errors.isEmpty()) {
+            //re-render form if any errors
+            res.json({ alerts: errors.array() })
+            return
+        } else {
+            // successful redirect to update category detail page
+            Post.findByIdAndUpdate(req.params.postid, post, {}, function (err) {
+                if (err) { return next(err) }
+
+                res.json({
+                    alerts: [{ msg: "Post Updated Successfully" }],
+                    post
+                });
+            })
         };
     }
 ];
