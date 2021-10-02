47749fb4e45d599c716aa9e8c30d3c9e087434e9
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-02 13:08:15 +0100

    added models folder and UserSchema

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    profilePicUrl: { type: String, required: true },
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }]
+});
+
+module.exports = mongoose.model('User', UserSchema);
