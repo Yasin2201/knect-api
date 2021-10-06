197cc70c582131d6351aae6d7fba3da48e726bf3
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-06 15:47:09 +0100

    add jwt secure routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const passport = require("passport");
+
 const user_controller = require('../controllers/userController');
 const post_controller = require('../controllers/postController');
 const comment_controller = require('../controllers/commentController');
 const friendRequest_controller = require('../controllers/friendrequestController');
-const friendRequest = require('../models/friendRequest');
 
 /*
     USERS ROUTES
@@ -13,7 +14,10 @@ const friendRequest = require('../models/friendRequest');
 router.post('/sign-up', user_controller.sign_up_post)
 
 //POST user sign-in 
-router.post('/', user_controller.sign_in_post)
+router.post('/sign-in', user_controller.sign_in_post)
+
+//GET sign-in page
+router.get('/', user_controller.sign_in_get)
 
 //GET user sign-out
 router.get('/sign-out', user_controller.sign_out_get)
@@ -22,58 +26,58 @@ router.get('/sign-out', user_controller.sign_out_get)
     POSTS ROUTES
 */
 //POST new post
-router.post('/:id', post_controller.new_post)
+router.post('/:id/new-post', passport.authenticate('jwt', { session: false }), post_controller.new_post)
 
 //GET all users posts
-router.get('/:id/posts', post_controller.get_all_posts)
+router.get('/:id/posts', passport.authenticate('jwt', { session: false }), post_controller.get_all_posts)
 
 //GET single post
-router.get('/:userid/posts/:postid', post_controller.get_post)
+router.get('/:userid/posts/:postid', passport.authenticate('jwt', { session: false }), post_controller.get_post)
 
 //UPDATE post PUT
-router.put('/:userid/update-post/:postid', post_controller.update_post)
+router.put('/:userid/update-post/:postid', passport.authenticate('jwt', { session: false }), post_controller.update_post)
 
 //PUT like/unlike post
-router.put('/:userid/like-post/:postid', post_controller.like_post)
+router.put('/:userid/like-post/:postid', passport.authenticate('jwt', { session: false }), post_controller.like_post)
 
 //DELETE a post
-router.delete('/:userid/delete-post/:postid', post_controller.delete_post)
+router.delete('/:userid/delete-post/:postid', passport.authenticate('jwt', { session: false }), post_controller.delete_post)
 
 /*
     COMMENTS ROUTES
 */
 //POST new comment
-router.post('/:userid/post/:postid/comment', comment_controller.new_comment)
+router.post('/:userid/post/:postid/comment', passport.authenticate('jwt', { session: false }), comment_controller.new_comment)
 
 //GET all posts comments
-router.get('/:id/comments', comment_controller.get_post_comments)
+router.get('/:id/comments', passport.authenticate('jwt', { session: false }), comment_controller.get_post_comments)
 
 //UPDATE comment
-router.put('/:userid/update-comment/:commentid', comment_controller.update_comment)
+router.put('/:userid/update-comment/:commentid', passport.authenticate('jwt', { session: false }), comment_controller.update_comment)
 
 //PUT like/unlike post
-router.put('/:userid/like-comment/:commentid', comment_controller.like_comment)
+router.put('/:userid/like-comment/:commentid', passport.authenticate('jwt', { session: false }), comment_controller.like_comment)
 
 //DELETE comment
-router.delete('/:userid/delete-comment/:commentid', comment_controller.delete_comment)
+router.delete('/:userid/delete-comment/:commentid', passport.authenticate('jwt', { session: false }), comment_controller.delete_comment)
 
 
 /*
     FRIEND REQUEST ROUTES
 */
 //GET all friend requests for user
-router.get('/:id/requests', friendRequest_controller.get_all_requests)
+router.get('/:id/requests', passport.authenticate('jwt', { session: false }), friendRequest_controller.get_all_requests)
 
 //POST new friend request
-router.post('/:userid/request/:recid', friendRequest_controller.new_friend_request)
+router.post('/:userid/request/:recid', passport.authenticate('jwt', { session: false }), friendRequest_controller.new_friend_request)
 
 //DELETE or decline a friend request
-router.delete('/:userid/request/:requestid', friendRequest_controller.decline_friend_Request)
+router.delete('/:userid/request/:requestid', passport.authenticate('jwt', { session: false }), friendRequest_controller.decline_friend_Request)
 
 //UPDATE friend request status on acceptance
-router.put('/:userid/request/:requestid', friendRequest_controller.accept_friend_request)
+router.put('/:userid/request/:requestid', passport.authenticate('jwt', { session: false }), friendRequest_controller.accept_friend_request)
 
 //DELETE users friendRequest document and remove friends from each users array when user unfriends
-router.delete('/:userid/unfriend/:friendid', friendRequest_controller.unfriend_user)
+router.delete('/:userid/unfriend/:friendid', passport.authenticate('jwt', { session: false }), friendRequest_controller.unfriend_user)
 
 module.exports = router;
