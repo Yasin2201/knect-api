1686e73f4c4f34e772f30bec36dcb8782096a3f9
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-13 11:49:49 +0100

    added user to comment and post models

--- models/comment.js
+++ models/comment.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const CommentSchema = new Schema({
     postId: { type: Schema.Types.ObjectId, ref: "Post" },
     userId: { type: Schema.Types.ObjectId, ref: "User" },
+    username: { type: String, required: true },
     text: { type: String, minLength: 1, required: true },
     date: { type: Date, default: Date.now },
     likes: [{ type: Schema.Types.ObjectId, ref: "User" }]
--- models/post.js
+++ models/post.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
+    username: { type: String, required: true },
     userId: { type: Schema.Types.ObjectId, ref: "User" },
     text: { type: String, minLength: 1, required: true },
     date: { type: Date, default: Date.now },
